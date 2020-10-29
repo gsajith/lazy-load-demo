a6fc2d136ef0ebd7fdec8c09e1fb1ed8e0264c60
Gautham Sajith <gsajith@umich.edu>
2020-10-28 21:35:24 -0400

    Add favicon

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Lazy Loading Images</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>Hey!</div>
