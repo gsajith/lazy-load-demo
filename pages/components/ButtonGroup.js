1fd00be30f6395d5e89c25932c3ecbba5ea0403d
Gautham Sajith <gsajith@umich.edu>
2020-10-29 16:54:14 -0400

    Fix render issue, thanks Vercel bot

--- pages/components/ButtonGroup.js
+++ pages/components/ButtonGroup.js
@@ -5,26 +5,27 @@ import Button from './Button';
 const ButtonGroup = ({ options, selected, setSelected }) => {
   return (
     <Flex row style={{ marginTop: 16 }}>
-      {options.map((option, index) => {
-        let style = {};
-        if (index === 0 && options.length > 1) {
-          style.borderRadius = '8px 0px 0px 8px';
-        } else if (index === options.length - 1 && options.length > 1) {
-          style.borderRadius = '0px 8px 8px 0px';
-        } else if (options.length > 1) {
-          style.borderRadius = 0;
-        }
+      {options &&
+        options.map((option, index) => {
+          let style = {};
+          if (index === 0 && options.length > 1) {
+            style.borderRadius = '8px 0px 0px 8px';
+          } else if (index === options.length - 1 && options.length > 1) {
+            style.borderRadius = '0px 8px 8px 0px';
+          } else if (options.length > 1) {
+            style.borderRadius = 0;
+          }
 
-        return (
-          <Button
-            outlined={option !== selected}
-            style={style}
-            onClick={() => setSelected(option)}
-          >
-            {option}
-          </Button>
-        );
-      })}
+          return (
+            <Button
+              outlined={option !== selected}
+              style={style}
+              onClick={() => setSelected(option)}
+            >
+              {option}
+            </Button>
+          );
+        })}
     </Flex>
   );
 };
